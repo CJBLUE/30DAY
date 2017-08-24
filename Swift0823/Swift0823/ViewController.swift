0ba412c3a56989632677b1fb2b2638852157c34a
CJBLUE <18341911386@163.com>
2017-08-24 15:52:06 +0800

    changeFont

--- Swift0823/Swift0823/ViewController.swift
+++ Swift0823/Swift0823/ViewController.swift
@@ -67,7 +67,6 @@ class ViewController: UIViewController {
         }
     }
     
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
